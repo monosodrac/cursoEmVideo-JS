662e0eb9eea518a3eac10e6f4387daf62b1a03bd
Mono <rickyblesstf@gmail.com>
2024-05-04 21:56:13 -0300

    Aula14ex commit

---
+++ Aula14ex/ex016p/script.js
@@ -0,0 +1,27 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pas = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
+    if (ini.value.length == 0||fim.value.length == 0||pas.value.length == 0) {
+        res.innerHTML = 'Impossivel contar'
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (p <= 0) {
+            alert('Passo invalido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i;c <= f;c += p) {
+                res.innerHTML += ` ${c} `
+            }
+        } else {
+            for (let c = i;c >= f;c -= p) {
+                res.innerHTML += ` ${c} `
+            }
+        }
+    }
+}
---
+++ Aula14ex/ex017/script.js
@@ -0,0 +1,18 @@
+function gerar() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
